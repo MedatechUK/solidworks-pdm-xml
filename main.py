ae9fbb9edb8af072f8bd59694fa362f538e5adb9
Medatech <Medatech@gamecomp.local>
2022-10-27 09:22:18 -0500

    File listener

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from xml.etree import ElementTree as ET
 import pyodbc
 from datetime import datetime, timedelta
 import uuid
-import os
+import os, logging
 import shutil
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=PRIORITYSQL\PRI;'
@@ -12,6 +16,11 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'PWD=(game)T4bul4!')
 
 cursor = conn.cursor()
+
+def start_logging():
+    path = r"error.log"
+    logging.basicConfig(filename=path, level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info('ECO EPDM started.')
 
 def get_pri_time():
     fmt = '%d/%m/%y'
@@ -65,7 +74,7 @@ def parse_xml(path):
 
     # print("---- Parent attributes ----")
     attributes = get_attributes(configuration)
-    print(attributes)
+    # print(attributes)
 
     # datetime(year=2017, month=3, day=1, hour=0, minute=0, second=1)
     max_line = get_max_line()
@@ -119,22 +128,59 @@ def parse_xml(path):
             cursor.execute(sql, val)
             # conn.commit()  
 
-    conn.commit() 
+    conn.commit()
+    logging.info(f"File {path} parsed and loaded into table.")
 
 def handle_files():
+    start_logging()
     input_dir = os.fsencode('XML-Input')
 
     for file in os.listdir(input_dir):
         filename = os.fsdecode(file)
         if filename.endswith(".XML"):
+            logging.info(f"Parsing file {filename}.")
             parse_xml(os.path.join('XML-Input', filename))
             path_to_current_file = os.path.join('XML-Input', filename)
             path_to_new_file = os.path.join('XML-Loaded', filename)
             shutil.move(path_to_current_file, path_to_new_file)
 
-if __name__ == "__main__":
-    # parse_xml("XML-Input/GB1-3910-15-00.XML")
+def run_load_proc():
+    os.system('cmd /c "E:/Priority/bin.95/winrun "" tabula (game)T4bul4! E:\Priority\system\prep demo WINACTIV -P ZSFDC_LOADECO_M"')
+
+def folder_event_handler():
+    patterns = ["*"]
+    ignore_patterns = None
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler.on_created = on_created
+    return my_event_handler
+
+def folder_observer(my_event_handler):
+    path = "./XML-Input"
+    go_recursively = True
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+    return my_observer
+
+
+def on_created(event):
+    print(f"Detected file: {event.src_path}.")
     handle_files()
+    run_load_proc()
+
+if __name__ == "__main__":
+    my_observer = folder_observer(folder_event_handler())
+    my_observer.start()
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        my_observer.join()
+    # parse_xml("XML-Input/GB1-3910-15-00.XML")
+    # handle_files()
+    # run_load_proc()
 
 #TODO: Error logging
 # Create a procedure in priority that looks if the file exists in a given location and pass the filename
